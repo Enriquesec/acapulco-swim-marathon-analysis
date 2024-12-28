92030b808679a3aadb298017d5dec8ccbc37c6b6
Enriquesec <enriquesec@gmail.com>
2024-12-28 09:31:27 -0600

    Notebooks y constantes

---
+++ src/data/constants.py
@@ -0,0 +1,7 @@
+COLUMNS_MARATON = [
+    'lugar_categoría', 'lugar_rama', 'lugar_general',
+    'nombre_completo', 'número_de_competidor', 'procedencia', 'equipo',
+    'categoria', 'distancia', 'sexo', 'edad',
+    'time_hours', 'time_minutes',
+    'evento', 'id_resultado'
+    ]
