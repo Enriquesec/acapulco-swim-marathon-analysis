119392bf0bcb2b5853eb47146a51a6de3251383e
enriquesec <enriquesec@gmail.com>
2025-11-17 18:54:20 -0600

    Se eliminan los codigos de python, se mueven a otro repositorio

--- src/data/constants.py
+++
@@ -1,7 +0,0 @@
-COLUMNS_MARATON = [
-    'lugar_categoría', 'lugar_rama', 'lugar_general',
-    'nombre_completo', 'número_de_competidor', 'procedencia', 'equipo',
-    'categoria', 'distancia', 'sexo', 'edad',
-    'time_hours', 'time_minutes',
-    'evento', 'id_resultado'
-    ]
